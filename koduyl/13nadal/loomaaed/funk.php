e360f2dc4027cf695d235b676b3e732305f045cc
liina laumets <liina.laumets@itcollege.ee>
2017-05-16 11:05:25 +0300

    lisa

--- koduyl/13nadal/loomaaed/funk.php
+++ koduyl/13nadal/loomaaed/funk.php
@@ -13,6 +13,40 @@ function connect_db(){
 
 function logi(){
 	// siia on vaja funktsionaalsust (13. nädalal)
+	 if (isset($_POST['user'])) {
+		include_once('views/puurid.html');
+	}
+	//kontrollib, kas kasutaja on juba vormi saata üritanud. Kas päring on tehtud POST (vormi esitamisel) või GET (lingilt tulles) meetodil, saab teada serveri infost, mis asub massiivist $_SERVER võtmega 'REQUEST_METHOD'
+	if (isset($_SERVER['REQUEST_METHOD'])) {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		  	
+		  	//Kui meetodiks oli POST, kontrollida kas vormiväljad olid täidetud. Vastavalt vajadusele tekitada veateateid (massiiv $errors)
+		  	$errors = array();
+		  	if (empty($_POST['user']) || empty($_POST['pass'])) {
+		  		if(empty($_POST['user'])) {
+			    	$errors[] = "kasutajanimi on puudu";
+				}
+				if(empty($_POST['pass'])) {
+					$errors[] = "parool on puudu";
+				} 
+		  	} else {
+		  		//kui kõik väljad olid täidetud, üritada andmebaasitabelist <sinu kasutajanimi/kood/>_kylalised selekteerida külalist, kelle kasutajanimi ja parool on vastavad 
+		  		global $connection;
+		  		$username = mysqli_real_escape_string($connection, $_POST["user"]);
+		  		$passw = mysqli_real_escape_string($connection, $_POST["pass"]);
+		  		
+				$query = "SELECT id FROM llaumets_kylastajad WHERE username='$username' && passw=SHA1('$passw')";
+				$result = mysqli_query($connection, $query) or die("midagi läks valesti");
+			
+				//Kui selle SELECT päringu tulemuses on vähemalt 1 rida (seda saab teada mysqli_num_rows funktsiooniga) siis lugeda kasutaja sisselogituks -> luua sessiooniväli 'user' ning suunata ta loomaaia vaatesse
+				$ridu = mysqli_num_rows($result);
+					if ( $ridu > 0) {
+						$_SESSION['user'] = $username;
+						header("Location: ?page=loomad");
+					}
+		  	}
+		//igasuguste vigade korral ning lehele esmakordselt saabudes kuvatakse kasutajale sisselogimise vorm failist login.html
+		} else {
 
 	include_once('views/login.html');
 }
@@ -44,9 +78,43 @@ global $connection;
 
 function lisa(){
 	// siia on vaja funktsionaalsust (13. nädalal)
+	if (empty($_SESSION['user'])) {
+		include_once 'views/loomavorm.html';
 	
+}
+
+
+	//kontrollib, kas kasutaja on üritanud juba vormi saata. Kas päring on tehtud POST (vormi esitamisel) või GET (lingilt tulles) meetodil, saab teada serveri infost, mis asub massiivist $_SERVER võtmega 'REQUEST_METHOD'
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		//kui meetodiks oli POST, tuleb kontrollida, kas kõik vormiväljad olid täidetud ja tekitada vajadusel vastavaid veateateid (massiiv $errors). 
+		$errors = array();
+  	
+  		if(empty($_POST['nimi'])) {
+	    	$errors[] = "nimi on puudu";
+		}
+		if(empty($_POST['puur'])) {
+			$errors[] = "puur on puudu";
+		}
+		
+		$pilt = upload("liik");
+		if ($pilt == "") {
+			$errors[] = "pilt on puudu";
+		}
+	  	if (empty($errors)) {
+	  		//Kui vigu polnud, siis üritada see loom andmebaasitabelisse <sinu kasutajanimi/kood/>_loomaaed lisada. 
+	  		global $connection;
+	  		$loomanimi = mysqli_real_escape_string($connection, $_POST["nimi"]);
+	  		$puurinr = mysqli_real_escape_string($connection, $_POST["puur"]);
+			$query = "INSERT INTO eprangel_loomaaed (nimi, liik, puur) VALUES ('$loomanimi', '$pilt', '$puurinr')";
+			$result = mysqli_query($connection, $query) or die("midagi läks valesti");;
+		
+			//Kas looma lisamine õnnestus või mitte, saab teada kui kontrollida mis väärtuse tagastab mysqli_insert_id funktsioon. Kui väärtus on nullist suurem, suunata kasutaja loomade vaatessse 
+			if (mysqli_insert_id($connection) > 0) {
+				header("Location: ?page=loomad");
+			}
+	  	} 
+	}
 	include_once('views/loomavorm.html');
-	
 }
 
 function upload($name){
